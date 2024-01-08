b6d32c61b59d52c75b68387bc25b2766ce98dca9
Roxpj <148432952+Roxpj@users.noreply.github.com>
2024-01-08 10:19:30 +0100

    added dark mode variant to elements

--- scripts/dom.js
+++ scripts/dom.js
@@ -132,10 +132,30 @@ function loadTasks() {
 const darkModeToggleButton = document.querySelector('.toggle');
 
 function toggleDarkMode(ev){
+  let tasks = document.querySelectorAll(".new-task");
+  let checkboxes = document.querySelectorAll(".checkbox");
+  let deleteButtons = document.querySelectorAll(".delete");
+
+ 
   if(ev.target.checked){
-      document.body.removeAttribute('class', 'dark-mode');
+      document.body.classList.remove('dark-mode');
+      document.querySelector('#input-new-task').classList.remove('dark-mode');
+      document.querySelector('#input-new-task__description').classList.remove('dark-mode');
+      document.querySelector('#button-input').classList.remove('dark-mode');
+      document.querySelector("#task-container").classList.remove('dark-mode');
+     tasks.forEach((task) => task.classList.remove('dark-mode'));
+     checkboxes.forEach((checkbox) => checkbox.classList.remove('dark-mode'));
+     deleteButtons.forEach((deleteButton) => deleteButton.classList.remove('dark-mode'));
+
   } else {
-      document.body.setAttribute('class', 'dark-mode');
+    document.body.setAttribute('class', 'dark-mode');
+    document.querySelector('#input-new-task').classList.add('dark-mode');
+    document.querySelector('#input-new-task__description').classList.add('dark-mode');
+    document.querySelector('#button-input').classList.add('dark-mode');
+    document.querySelector("#task-container").classList.add('dark-mode');
+    tasks.forEach((task) => task.classList.add('dark-mode'));
+    checkboxes.forEach((checkbox) => checkbox.classList.add('dark-mode'));
+    deleteButtons.forEach((deleteButton) => deleteButton.classList.add('dark-mode'));
   }
 }
 darkModeToggleButton.addEventListener('click', toggleDarkMode);
